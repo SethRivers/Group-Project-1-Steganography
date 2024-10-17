873fb0b4e8cc67bd2ba6d325716d47373e9d50bb
Seth Rivers <davmrive@ut.utm.edu>
2024-10-17 01:50:09 -0500

    Initialized the .cpp file and readied most if not all functions for filling

---
+++ Steganography.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file Steganography.cpp
+ * @author Script Sages
+ * @date 2024-10-17
+ * @brief The cpp file for holding the guts of the file. 
+ * 
+ * 
+ */
+
+#include "Steganography.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+//which is the constructor/destructor? didnt see one in the .h 
+
+int getNthbit(char cipherChar, int n){
+
+};
+
+void readImage(const std::string& fileName){
+
+};
+
+void printImage(const std::string& fileName){
+
+};
+
+void PrintCipherText(const std::string& fileName){
+
+};
+
+void cleanImage(){
+
+};
+
+void encipher(){
+
+};
+
+void decipher(){
+
+};
