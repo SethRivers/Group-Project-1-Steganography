c89ca40f6a4bb27e6360c9557742bb7fd743f23c
Seth Rivers <davmrive@ut.utm.edu>
2024-10-17 23:03:48 -0500

    added some checklist notes

--- Steganography.cpp
+++ Steganography.cpp
@@ -20,16 +20,19 @@ int Steganography::getNthbit(char cipherChar, int n){
 
 };
 
+//need to read in the file, seperate the header from the thing(nested loop with an if statement?), then establish a system to scan in the ppm data. 
 void Steganography::readImage(const std::string& fileName){ 1
     ifstream file;
   string ppmName = "";
   
 };
 
+//sends the data to the file
 void Steganography::printImage(const std::string& fileName){ 1
 
 };
 
+//some kind of string reader to extract the file data and store it (maybe have something built in to convert the text into the boolean)
 void Steganography::readCipherText(string fileName){ 1
 
 }; 
@@ -38,14 +41,17 @@ void Steganography::printCipherText(const std::string& fileName){ 1
 
 };
 
+//readies the image for ciphering - will need a string reader that'll run simple math at the end of every 1-3 digit number
 void Steganography::cleanImage(){
 
 };
 
+//will add the 1s and 0s to the image data
 void Steganography::encipher(){
 
 };
 
+//will extract the data as necesary
 void Steganography::decipher(){
 
 };
